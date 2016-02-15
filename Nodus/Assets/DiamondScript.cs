55493b5ca1ca316a672a0380c41fc9b9820a4a8a
Iain Dowling <iain.dowling@ad.aie.edu>
2016-02-15 11:52:55 +1100

    Diamonds repeat pattern

---
+++ Nodus/Assets/DiamondScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiamondScript : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (transform.position.y < player.transform.position.y - 7)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 16, 0);
+        }
+
+	}
+}
